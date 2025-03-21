e0afc451cc400f229cbd96cdf3f376812e465053
Silva Mauricio Nicolás <113395913+SilvaMauricioN@users.noreply.github.com>
2025-03-21 18:21:20 -0300

    Componente Boton Registro

---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Boton = ({ texto }) => {
+  return <button className="bg-black text-white p-2 rounded w-full">{texto}</button>;
+};
+
+export default Boton;
--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Campo from "./Campo";
-// import PasswordField from "./PasswordField";
-// import Button from "./Button";
+import Boton from "./Boton";
+
 
 const Formulario = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-        <h2 className="text-xl font-bold text-center">Create an account</h2>
-        <p className="text-gray-500 text-center mb-4">Enter your information to register</p>
+        <h2 className="text-xl font-bold text-center">Crea una cuenta</h2>
+        <p className="text-gray-500 text-center mb-4">Ingresa tus datos para registrarte</p>
         
         <form>
           <div className="grid grid-cols-2 gap-2">
-            <Campo tipo="text" texto="First name" />
-            <Campo tipo="text" texto="Last name" />
+            <Campo tipo="text" texto="Primer Nombre" />
+            <Campo tipo="text" texto="Apellido" />
           </div>
 
           <Campo tipo="email" texto="Email" className="mt-2" />
-          <Campo tipo="tel" texto="Phone" className="mt-2" />
+          <Campo tipo="tel" texto="Telefono" className="mt-2" />
+
+          <Boton texto="Registrar" />
+
        
         </form>
       </div>
