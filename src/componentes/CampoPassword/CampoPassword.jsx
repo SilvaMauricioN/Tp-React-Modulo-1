0acc9cdf15ae17c773768312f49f3859a5d42c70
Silva Mauricio Nicolás <113395913+SilvaMauricioN@users.noreply.github.com>
2025-03-25 00:12:46 -0300

    arreglo css campo password

--- src/componentes/CampoPassword/CampoPassword.jsx
+++ src/componentes/CampoPassword/CampoPassword.jsx
@@ -5,12 +5,12 @@ import "./CampoPassword.css"
 const CampoPassword = ({ texto,placeholder }) => {
   
 	return (
-		<div className="ContenedorPassword">
-			<label className="TituloCampo">{texto}</label>
-			<input type="password" placeholder={placeholder} className="EntradaPassword"/>
-			<button type="button" className="BotonOjo">
-				<Iconos.EYE className="Icono"/>
-			</button>		
+	<div className="ContenedorPassword">
+		<label className="TituloCampo">{texto}</label>
+		<input type="password" placeholder={placeholder} className="EntradaPassword"/>
+		<button type="button" className="BotonOjo">
+			<Iconos.EYE className="Icono"/>
+		</button>		
 	 </div>
 	);
 }
